75979b02a60326538b57c3ef9012d672fcf8894a
Jeki The Monkey <tonysidenko@gmail.com>
2021-07-27 20:34:25 +0200

    rm def cfg

--- Headers/DefaultFiles.hpp
+++ Headers/DefaultFiles.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "DefaultConfig.hpp"
 #include "Default media/Textures/Textures.hpp"
 #include "Default media/Fonts/TextFont.hpp"
 #include "Default media/Fonts/KPSWindowTextFont.hpp"
