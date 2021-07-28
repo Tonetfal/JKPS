dd56dd658ad2d57ce289591caf0949cdab71d5f5
Jeki The Monkey <tonysidenko@gmail.com>
2021-07-28 08:55:46 +0200

    Changed default media names

--- Headers/DefaultFiles.hpp
+++ Headers/DefaultFiles.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
 #include "Default media/Textures/Textures.hpp"
-#include "Default media/Fonts/TextFont.hpp"
+#include "Default media/Fonts/MainProgramFont.hpp"
 #include "Default media/Fonts/KPSWindowTextFont.hpp"
