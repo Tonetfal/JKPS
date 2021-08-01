49d19a69699fa752775e83dafbb759c7e749c270
Jeki The Monkey <tonysidenko@gmail.com>
2021-08-01 21:39:01 +0200

    Fixed button wrong collision

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -29,6 +29,7 @@ Application::Application(Menu &menu)
     mKPSWindow = std::move(kpsWindow);
 
     mSettings.setWindowReference(mWindow);
+    mSettings.setButtonsReference(*mButtons);
 }
 
 void Application::run()
@@ -235,7 +236,9 @@ void Application::openWindow()
 
     mWindow.create(sf::VideoMode(getWindowWidth(), getWindowHeight()), 
         "JKPS", Settings::WindowTitleBar ? sf::Style::Default : sf::Style::None);
-    mWindow.setPosition(sf::Vector2i(desktop.width / 2 - mWindow.getSize().x / 2, desktop.height / 2 - mWindow.getSize().y / 2));
+    mWindow.setPosition(sf::Vector2i(
+        desktop.width / 2  - mWindow.getSize().x / 2, 
+        desktop.height / 2 - mWindow.getSize().y / 2));
     mWindow.setKeyRepeatEnabled(false);
     mWindow.setFramerateLimit(60);
 }
