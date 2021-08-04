271380a2a1f541f1ace38e85bb2c8bbde2208532
Jeki The Monkey <tonysidenko@gmail.com>
2021-08-04 12:52:05 +0200

    Code refactoring. Made it work with new logical buttons, both keyboard and mouse

--- Headers/ConfigHelper.hpp
+++ Headers/ConfigHelper.hpp
@@ -3,26 +3,25 @@
 #include "LogicalParameter.hpp"
 #include "ParameterLine.hpp"
 #include "Button.hpp"
-#include "LogicalKey.hpp"
-#include "LogicalButton.hpp"
 
 #include <SFML/Window/Keyboard.hpp>
 #include <SFML/Window/Mouse.hpp>
 
 #include <string>
 #include <vector>
+#include <queue>
 #include <map>
 
 
 namespace ConfigHelper
 {
     
-void readConfig(
+bool readConfig(
     std::map<LogicalParameter::ID, std::shared_ptr<LogicalParameter>> &parameters, 
     std::map<ParameterLine::ID, std::shared_ptr<ParameterLine>> &parameterLines);
 void fillParameters(std::map<LogicalParameter::ID, std::shared_ptr<LogicalParameter>> &parameters);
-void fillKeys(std::vector<std::unique_ptr<LogicalKey>> &keys);
-void fillButtons(std::vector<std::unique_ptr<LogicalButton>> &buttons);
+std::queue<LogKey> getLogKeys();
+std::queue<LogKey> getLogButtons();
 std::string readParameter(const std::string &par, bool &parameterFound);
 void writeParameter(LogicalParameter &par);
 float readDigitParameter(const LogicalParameter &par, const std::string &valStr);
@@ -31,20 +30,18 @@ sf::Color readColorParameter(const LogicalParameter &par, const std::string &val
 bool readBoolParameter(const LogicalParameter &par, const std::string &valStr);
 void controlAssets(std::map<LogicalParameter::ID, std::shared_ptr<LogicalParameter>> &parameters);
 
-void readKeys(std::vector<std::unique_ptr<LogicalKey>> &keys, const std::string &keysStr, const std::string &visualKeysStr);
-bool isKeyAlreadyPresent(const std::vector<std::unique_ptr<LogicalKey>> &keys, sf::Keyboard::Key key, unsigned &cpyIdx, unsigned ignIdx);
-std::unique_ptr<LogicalKey> getDefaultLogicalKey(const std::vector<std::unique_ptr<LogicalKey>> &keys);
-sf::Keyboard::Key getDefaultKey(const std::vector<std::unique_ptr<LogicalKey>> &keys);
-
-void readButtons(std::vector<std::unique_ptr<LogicalButton>> &buttons, const std::string &valStr, const std::string &visualValStr);
-bool isButtonAlreadyPresent(const std::vector<std::unique_ptr<LogicalButton>> &buttons, sf::Mouse::Button button, unsigned &cpyIdx, unsigned ignIdx);
-std::unique_ptr<LogicalButton> getDefaultLogicalButton(const std::vector<std::unique_ptr<LogicalButton>> &keys);
-sf::Mouse::Button getDefaultButton(const std::vector<std::unique_ptr<LogicalButton>> &buttons);
+std::queue<LogKey> readKeys(const std::string &keysStr, const std::string &visualKeysStr);
+std::queue<LogKey> readButtons(const std::string &valStr, const std::string &visualValStr);
 
 void createDefaultConfig();
 void saveConfig(
     const std::map<LogicalParameter::ID, std::shared_ptr<LogicalParameter>> &parameters, 
-    const std::map<ParameterLine::ID, std::shared_ptr<ParameterLine>> &parameterLines);
+    const std::map<ParameterLine::ID, std::shared_ptr<ParameterLine>> &parameterLines,
+    const std::vector<std::unique_ptr<Button>> *mKeys,
+    bool saveKeys);
+
+std::string getKeysStr(const std::vector<std::unique_ptr<Button>> &mKeys, std::string str, bool readRealStr);
+std::string getButtonStr(const std::vector<std::unique_ptr<Button>> &mKeys, std::string str, bool readRealStr);
 
 extern const unsigned minKeys;
 extern const unsigned maxKeys;
