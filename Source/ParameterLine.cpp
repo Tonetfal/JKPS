f30e63ceb75b0f329da62c6107a9b903da3e9a29
Jeki The Monkey <tonysidenko@gmail.com>
2021-08-05 23:56:54 +0200

    Fixed collision issues, bugs with negative zeros and added new parameters

--- Source/ParameterLine.cpp
+++ Source/ParameterLine.cpp
@@ -6,6 +6,7 @@
 #include "../Headers/Settings.hpp"
 #include "../Headers/Menu.hpp"
 #include "../Headers/GfxButton.hpp"
+#include "../Headers/ConfigHelper.hpp"
 
 #include <SFML/Window/Event.hpp>
 #include <SFML/Graphics/RenderTarget.hpp>
@@ -99,6 +100,7 @@ void ParameterLine::handleValueModEvent(sf::Event event)
             str = mParameter->getValPt(btnIdx);
         }
 
+        std::cout << isStrType << "\n";
         if (!isStrType
         && ((key >= sf::Keyboard::Num0 &&  key <= sf::Keyboard::Num9)
         || (key >= sf::Keyboard::Numpad0 && key <= sf::Keyboard::Numpad9)))
@@ -112,10 +114,10 @@ void ParameterLine::handleValueModEvent(sf::Event event)
             if (mSelectedValueIndex == 0 && (str[0] == '-' || n == 0))
                 return;
 
-            if (str.size() == 1 && str[0] == '0' || 
-               (str.size() == 2 && str[1] == '0' && str[0] == '-'))
+            if (str.size() == 1 && str[0] == '0'
+            ||  str.size() == 2 && str[1] == '0' && str[0] == '-')
             {
-                str[str.size() - 1] = n + '0';
+                str.back() = n + '0';
                 --mSelectedValueIndex;
             }
             else
@@ -162,15 +164,14 @@ void ParameterLine::handleValueModEvent(sf::Event event)
             if (mType == LogicalParameter::Type::Unsigned || mType == LogicalParameter::Type::Color)
                 return;
 
-            if (str[0] != '-')
-            {
-                addChOnIdx(str, 0, '-');
-                ++mSelectedValueIndex;
-            }
-            else if (str[0] == '-')
+            if (str[0] == '-')
             {
                 rmChOnIdx(str, 0);
                 --mSelectedValueIndex;
+            } else if (str != "0")
+            {
+                addChOnIdx(str, 0, '-');
+                ++mSelectedValueIndex;
             }
         }
 
@@ -183,14 +184,16 @@ void ParameterLine::handleValueModEvent(sf::Event event)
                 {
                     str.resize(str.size() - 1);
                     str.append(mParameter->mLowLimits <= 0 ? "0" : std::to_string(static_cast<int>(mParameter->mLowLimits)));
+                    if (str.size() == 2 && str[0] == '-' && str[1] == '0')
+                        str = "0";
                     mSelectedValueIndex = str.size() + 1;
                 }
                 else
                 {
                     rmChOnIdx(str, mSelectedValueIndex - 1);
-                    // ex.: -500, remove 5, result "-0"
+                    // ex.: -500, remove 5, result "0"
                     if (!isStrType && std::stoi(str) == 0)
-                        str = std::string(str[0] == '-' ? "-" : "") + "0";
+                        str = "0";
                 }
 
                 --mSelectedValueIndex;
@@ -209,9 +212,9 @@ void ParameterLine::handleValueModEvent(sf::Event event)
                 else
                 {
                     rmChOnIdx(str, mSelectedValueIndex);
-                    // ex.: -500, remove 5, result "-0"
+                    // ex.: -500, remove 5, result "0"
                     if (!isStrType && std::stoi(str) == 0)
-                        str = std::string(str[0] == '-' ? "-" : "") + "0";
+                        str = "0";
                 }
             }
         }
@@ -279,8 +282,7 @@ void ParameterLine::handleButtonsInteractionEvent(sf::Event event)
         // Check click on buttons
         for (const auto &elem : mParameterValues)
         {
-            const sf::FloatRect buttonBounds(elem->getGlobalBounds());
-            const bool contains = buttonBounds.contains(absMousePos);
+            const bool contains = elem->contains(absMousePos);
 
             if (event.type == sf::Event::MouseButtonPressed 
             && (button == sf::Mouse::Left && contains))
@@ -368,11 +370,13 @@ bool ParameterLine::tabulation(sf::Keyboard::Key key)
 
 void ParameterLine::selectRgbCircle(sf::Mouse::Button button, sf::Vector2f mousePos)
 {
-    sf::FloatRect rgbCircleBounds = mColorButtonP->getTransform().transformRect(
-        mColorButtonP->rgbCircleSprite.getGlobalBounds());
-    rgbCircleBounds.top += getPosition().y;
+    const sf::Vector2f circleOrigin(mColorButtonP->getOrigin());
+    const sf::Vector2f circlePosition(mColorButtonP->getPosition());
+    sf::FloatRect circleRect(mColorButtonP->rgbCircleSprite.getGlobalBounds());
+    circleRect.left = getPosition().x + circlePosition.x - circleOrigin.x;
+    circleRect.top = getPosition().y + circlePosition.y - circleOrigin.y;
 
-    if (rgbCircleBounds.contains(mousePos))
+    if (circleRect.contains(mousePos))
     {
         // If there is already selected line, but it isn't this one
         if (mSelectedLine && !isItSelectedLine(shared_from_this()))
@@ -421,7 +425,7 @@ void ParameterLine::buildButtons(const std::string &valueStr, const FontHolder &
     std::shared_ptr<GfxParameter> val = nullptr; 
     if (mType == LogicalParameter::Type::Bool)
     {
-        val = std::make_shared<GfxParameter>(this, readValue(valueStr, 0));
+        val = std::make_shared<GfxParameter>(this, readValue(valueStr, 0) == "True");
         val->setPosition(sf::Vector2f(mRectLine.getSize().x - 
             GfxParameter::getPosX(), mRectLine.getSize().y / 2));
         mParameterValues.emplace_back(std::move(val));
@@ -457,6 +461,7 @@ void ParameterLine::buildButtons(const std::string &valueStr, const FontHolder &
             float dist = 65.f;
             mColorButtonP = std::make_unique<ColorButton>(textures.get(Textures::rgbCircle));
             mColorButtonP->setPosition(val->getPosition().x - dist, mRectLine.getSize().y / 2);
+            mColorButtonP->setOrigin(static_cast<sf::Vector2f>(mColorButtonP->rgbCircleSprite.getTexture()->getSize()) / 2.f);
         }
 
         mParameterValues.emplace_back(std::move(val));
@@ -684,10 +689,39 @@ ParameterLine::ID ParameterLine::parIdToParLineId(LogicalParameter::ID id)
         case LogicalParameter::ID::StatTextShowKPS: return ParameterLine::ID::StatTextShowKPS;
         case LogicalParameter::ID::StatTextShowTotal: return ParameterLine::ID::StatTextShowTotal;
         case LogicalParameter::ID::StatTextShowBPM: return ParameterLine::ID::StatTextShowBPM;
+        case LogicalParameter::ID::StatTextPosAdvMode: return ParameterLine::ID::StatTextPosAdvMode;
+        case LogicalParameter::ID::StatTextPos1: return ParameterLine::ID::StatTextPos1;
+        case LogicalParameter::ID::StatTextPos2: return ParameterLine::ID::StatTextPos2;
+        case LogicalParameter::ID::StatTextPos3: return ParameterLine::ID::StatTextPos3;
+        case LogicalParameter::ID::StatTextClrAdvMode: return ParameterLine::ID::StatTextClrAdvMode;
+        case LogicalParameter::ID::StatTextClr1: return ParameterLine::ID::StatTextClr1;
+        case LogicalParameter::ID::StatTextClr2: return ParameterLine::ID::StatTextClr2;
+        case LogicalParameter::ID::StatTextClr3: return ParameterLine::ID::StatTextClr3;
+        case LogicalParameter::ID::StatTextChSzAdvMode: return ParameterLine::ID::StatTextChSzAdvMode;
+        case LogicalParameter::ID::StatTextChSz1: return ParameterLine::ID::StatTextChSz1;
+        case LogicalParameter::ID::StatTextChSz2: return ParameterLine::ID::StatTextChSz2;
+        case LogicalParameter::ID::StatTextChSz3: return ParameterLine::ID::StatTextChSz3;
+        case LogicalParameter::ID::StatTextBoldAdvMode: return ParameterLine::ID::StatTextBoldAdvMode;
+        case LogicalParameter::ID::StatTextBold1: return ParameterLine::ID::StatTextBold1;
+        case LogicalParameter::ID::StatTextBold2: return ParameterLine::ID::StatTextBold2;
+        case LogicalParameter::ID::StatTextBold3: return ParameterLine::ID::StatTextBold3;
+        case LogicalParameter::ID::StatTextItalAdvMode: return ParameterLine::ID::StatTextItalAdvMode;
+        case LogicalParameter::ID::StatTextItal1: return ParameterLine::ID::StatTextItal1;
+        case LogicalParameter::ID::StatTextItal2: return ParameterLine::ID::StatTextItal2;
+        case LogicalParameter::ID::StatTextItal3: return ParameterLine::ID::StatTextItal3;
+        case LogicalParameter::ID::StatTextKPSText: return ParameterLine::ID::StatTextKPSText;
+        case LogicalParameter::ID::StatTextKPS2Text: return ParameterLine::ID::StatTextKPS2Text;
+        case LogicalParameter::ID::StatTextTotalText: return ParameterLine::ID::StatTextTotalText;
+        case LogicalParameter::ID::StatTextBPMText: return ParameterLine::ID::StatTextBPMText;
+
         case LogicalParameter::ID::BtnTextFont: return ParameterLine::ID::BtnTextFont;
         case LogicalParameter::ID::BtnTextClr: return ParameterLine::ID::BtnTextClr;
         case LogicalParameter::ID::BtnTextChSz: return ParameterLine::ID::BtnTextChSz;
         case LogicalParameter::ID::BtnTextPosition: return ParameterLine::ID::BtnTextPosition;
+        case LogicalParameter::ID::BtnTextVisPosition: return ParameterLine::ID::BtnTextVisPosition;
+        case LogicalParameter::ID::BtnTextTotPosition: return ParameterLine::ID::BtnTextTotPosition;
+        case LogicalParameter::ID::BtnTextKPSPosition: return ParameterLine::ID::BtnTextKPSPosition;
+        case LogicalParameter::ID::BtnTextBPMPosition: return ParameterLine::ID::BtnTextBPMPosition;
         case LogicalParameter::ID::BtnTextBounds: return ParameterLine::ID::BtnTextBounds;
         case LogicalParameter::ID::BtnTextBold: return ParameterLine::ID::BtnTextBold;
         case LogicalParameter::ID::BtnTextItal: return ParameterLine::ID::BtnTextItal;
@@ -695,10 +729,63 @@ ParameterLine::ID ParameterLine::parIdToParLineId(LogicalParameter::ID id)
         case LogicalParameter::ID::BtnTextShowTot: return ParameterLine::ID::BtnTextShowTot;
         case LogicalParameter::ID::BtnTextShowKps: return ParameterLine::ID::BtnTextShowKps;
         case LogicalParameter::ID::BtnTextShowBpm: return ParameterLine::ID::BtnTextShowBpm;
+
+        case LogicalParameter::ID::BtnTextPosAdvMode: return ParameterLine::ID::BtnTextPosAdvMode;
+        case LogicalParameter::ID::BtnTextPos1:  return ParameterLine::ID::BtnTextPos1;
+        case LogicalParameter::ID::BtnTextPos2:  return ParameterLine::ID::BtnTextPos2;
+        case LogicalParameter::ID::BtnTextPos3:  return ParameterLine::ID::BtnTextPos3;
+        case LogicalParameter::ID::BtnTextPos4:  return ParameterLine::ID::BtnTextPos4;
+        case LogicalParameter::ID::BtnTextPos5:  return ParameterLine::ID::BtnTextPos5;
+        case LogicalParameter::ID::BtnTextPos6:  return ParameterLine::ID::BtnTextPos6;
+        case LogicalParameter::ID::BtnTextPos7:  return ParameterLine::ID::BtnTextPos7;
+        case LogicalParameter::ID::BtnTextPos8:  return ParameterLine::ID::BtnTextPos8;
+        case LogicalParameter::ID::BtnTextPos9:  return ParameterLine::ID::BtnTextPos9;
+        case LogicalParameter::ID::BtnTextPos10: return ParameterLine::ID::BtnTextPos10;
+        case LogicalParameter::ID::BtnTextPos11: return ParameterLine::ID::BtnTextPos11;
+        case LogicalParameter::ID::BtnTextPos12: return ParameterLine::ID::BtnTextPos12;
+        case LogicalParameter::ID::BtnTextPos13: return ParameterLine::ID::BtnTextPos13;
+        case LogicalParameter::ID::BtnTextPos14: return ParameterLine::ID::BtnTextPos14;
+        case LogicalParameter::ID::BtnTextPos15: return ParameterLine::ID::BtnTextPos15;
+
+
         case LogicalParameter::ID::BtnGfxDist: return ParameterLine::ID::BtnGfxDist;
         case LogicalParameter::ID::BtnGfxTxtr: return ParameterLine::ID::BtnGfxTxtr;
         case LogicalParameter::ID::BtnGfxTxtrSz: return ParameterLine::ID::BtnGfxTxtrSz;
         case LogicalParameter::ID::BtnGfxTxtrClr: return ParameterLine::ID::BtnGfxTxtrClr;
+        case LogicalParameter::ID::BtnGfxBtnPosAdvMode: return ParameterLine::ID::BtnGfxBtnPosAdvMode;
+        case LogicalParameter::ID::BtnGfxSzAdvMode: return ParameterLine::ID::BtnGfxSzAdvMode;
+        case LogicalParameter::ID::BtnGfxBtnPos1: return ParameterLine::ID::BtnGfxBtnPos1;
+        case LogicalParameter::ID::BtnGfxSz1: return ParameterLine::ID::BtnGfxSz1;
+        case LogicalParameter::ID::BtnGfxBtnPos2: return ParameterLine::ID::BtnGfxBtnPos2;
+        case LogicalParameter::ID::BtnGfxSz2: return ParameterLine::ID::BtnGfxSz2;
+        case LogicalParameter::ID::BtnGfxBtnPos3: return ParameterLine::ID::BtnGfxBtnPos3;
+        case LogicalParameter::ID::BtnGfxSz3: return ParameterLine::ID::BtnGfxSz3;
+        case LogicalParameter::ID::BtnGfxBtnPos4: return ParameterLine::ID::BtnGfxBtnPos4;
+        case LogicalParameter::ID::BtnGfxSz4: return ParameterLine::ID::BtnGfxSz4;
+        case LogicalParameter::ID::BtnGfxBtnPos5: return ParameterLine::ID::BtnGfxBtnPos5;
+        case LogicalParameter::ID::BtnGfxSz5: return ParameterLine::ID::BtnGfxSz5;
+        case LogicalParameter::ID::BtnGfxBtnPos6: return ParameterLine::ID::BtnGfxBtnPos6;
+        case LogicalParameter::ID::BtnGfxSz6: return ParameterLine::ID::BtnGfxSz6;
+        case LogicalParameter::ID::BtnGfxBtnPos7: return ParameterLine::ID::BtnGfxBtnPos7;
+        case LogicalParameter::ID::BtnGfxSz7: return ParameterLine::ID::BtnGfxSz7;
+        case LogicalParameter::ID::BtnGfxBtnPos8: return ParameterLine::ID::BtnGfxBtnPos8;
+        case LogicalParameter::ID::BtnGfxSz8: return ParameterLine::ID::BtnGfxSz8;
+        case LogicalParameter::ID::BtnGfxBtnPos9: return ParameterLine::ID::BtnGfxBtnPos9;
+        case LogicalParameter::ID::BtnGfxSz9: return ParameterLine::ID::BtnGfxSz9;
+        case LogicalParameter::ID::BtnGfxBtnos10: return ParameterLine::ID::BtnGfxBtnos10;
+        case LogicalParameter::ID::BtnGfxSz10: return ParameterLine::ID::BtnGfxSz10;
+        case LogicalParameter::ID::BtnGfxBtnos11: return ParameterLine::ID::BtnGfxBtnos11;
+        case LogicalParameter::ID::BtnGfxSz11: return ParameterLine::ID::BtnGfxSz11;
+        case LogicalParameter::ID::BtnGfxBtnos12: return ParameterLine::ID::BtnGfxBtnos12;
+        case LogicalParameter::ID::BtnGfxSz12: return ParameterLine::ID::BtnGfxSz12;
+        case LogicalParameter::ID::BtnGfxBtnos13: return ParameterLine::ID::BtnGfxBtnos13;
+        case LogicalParameter::ID::BtnGfxSz13: return ParameterLine::ID::BtnGfxSz13;
+        case LogicalParameter::ID::BtnGfxBtnos14: return ParameterLine::ID::BtnGfxBtnos14;
+        case LogicalParameter::ID::BtnGfxSz14: return ParameterLine::ID::BtnGfxSz14;
+        case LogicalParameter::ID::BtnGfxBtnos15: return ParameterLine::ID::BtnGfxBtnos15;
+        case LogicalParameter::ID::BtnGfxSz15: return ParameterLine::ID::BtnGfxSz15;
+        
+
         case LogicalParameter::ID::AnimGfxLight: return ParameterLine::ID::AnimGfxLight;
         case LogicalParameter::ID::AnimGfxPress: return ParameterLine::ID::AnimGfxPress;
         case LogicalParameter::ID::AnimGfxTxtr: return ParameterLine::ID::AnimGfxTxtr;
@@ -706,14 +793,17 @@ ParameterLine::ID ParameterLine::parIdToParLineId(LogicalParameter::ID id)
         case LogicalParameter::ID::AnimGfxScl: return ParameterLine::ID::AnimGfxScl;
         case LogicalParameter::ID::AnimGfxClr: return ParameterLine::ID::AnimGfxClr;
         case LogicalParameter::ID::AnimGfxOffset: return ParameterLine::ID::AnimGfxOffset;
+
         case LogicalParameter::ID::BgTxtr: return ParameterLine::ID::BgTxtr;
         case LogicalParameter::ID::BgClr: return ParameterLine::ID::BgClr;
         case LogicalParameter::ID::BgScale: return ParameterLine::ID::BgScale;
+
         case LogicalParameter::ID::MainWndwTitleBar: return ParameterLine::ID::MainWndwTitleBar;
         case LogicalParameter::ID::MainWndwTop: return ParameterLine::ID::MainWndwTop;
         case LogicalParameter::ID::MainWndwBot: return ParameterLine::ID::MainWndwBot;
         case LogicalParameter::ID::MainWndwLft: return ParameterLine::ID::MainWndwLft;
         case LogicalParameter::ID::MainWndwRght: return ParameterLine::ID::MainWndwRght;
+
         case LogicalParameter::ID::KPSWndwEn: return ParameterLine::ID::KPSWndwEn;
         case LogicalParameter::ID::KPSWndwSz: return ParameterLine::ID::KPSWndwSz;
         case LogicalParameter::ID::KPSWndwTxtChSz: return ParameterLine::ID::KPSWndwTxtChSz;
@@ -725,28 +815,29 @@ ParameterLine::ID ParameterLine::parIdToParLineId(LogicalParameter::ID id)
         case LogicalParameter::ID::KPSWndwNumFont: return ParameterLine::ID::KPSWndwNumFont;
         case LogicalParameter::ID::KPSWndwTopPadding: return ParameterLine::ID::KPSWndwTopPadding;
         case LogicalParameter::ID::KPSWndwDistBtw: return ParameterLine::ID::KPSWndwDistBtw;
-        case LogicalParameter::ID::ThemeDevMultpl: return ParameterLine::ID::ThemeDevMultpl;
-        case LogicalParameter::ID::StatTextKPSText: return ParameterLine::ID::StatTextKPSText;
-        case LogicalParameter::ID::StatTextKPS2Text: return ParameterLine::ID::StatTextKPS2Text;
-        case LogicalParameter::ID::StatTextTotalText: return ParameterLine::ID::StatTextTotalText;
-        case LogicalParameter::ID::StatTextBPMText: return ParameterLine::ID::StatTextBPMText;
 
-        default: return ParameterLine::ID::StatTextColl;
+        case LogicalParameter::ID::ThemeDevMultpl: return ParameterLine::ID::ThemeDevMultpl;
+
+        // You forgot to add a parameter here, dumbass
+        default: assert(false); return ParameterLine::ID::StatTextColl;
     }
 }
 
-bool ParameterLine::isCollection(ParameterLine::ID id)
+bool ParameterLine::isEmpty(ParameterLine::ID id)
 {
     return
-        id == ParameterLine::ID::StatTextColl ||
-        id == ParameterLine::ID::BtnTextColl ||
-        id == ParameterLine::ID::BtnGfxColl ||
-        id == ParameterLine::ID::AnimGfxColl ||
-        id == ParameterLine::ID::BgColl ||
-        id == ParameterLine::ID::MainWndwColl ||
-        id == ParameterLine::ID::KPSWndwColl ||
-        id == ParameterLine::ID::InfoColl ||
-        id == ParameterLine::ID::HotkeyColl;
+        // id == ParameterLine::ID::StatTextMty ||
+        id == ParameterLine::ID::StatTextAdvMty ||
+        // id == ParameterLine::ID::BtnTextMty ||
+        id == ParameterLine::ID::BtnTextAdvMty ||
+        // id == ParameterLine::ID::BtnGfxMty ||
+        id == ParameterLine::ID::BtnGfxAdvMty ||
+        id == ParameterLine::ID::AnimGfxMty ||
+        id == ParameterLine::ID::MainWndwMty ||
+        id == ParameterLine::ID::KPSWndwMty ||
+        id == ParameterLine::ID::ThemeDevMty ||
+        // id == ParameterLine::ID::InfoMty ||
+        id == ParameterLine::ID::LastLine;
 }
 
 
